a6ab6dba371df506299c20ab71ac5d6809f3b7b9
Henry Mattoon <hmattoon@mail.sfsu.edu>
2020-03-27 02:30:57 -0700

    initial setup of express server

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const port = 3000;
+
+const app = express();
+
+
+app.use('/', express.static(path.join(__dirname, './../client/dist')));
+
+app.get('/api/test', (req, res) => {
+
+
+  res.end();
+})
+
+app.listen(port, ()=> (console.log('listening to port: ', port)));
