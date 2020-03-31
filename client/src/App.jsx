7fed68b207973afe3bc027a897085d6be432a048
Henry Mattoon <hmattoon@mail.sfsu.edu>
2020-03-31 08:13:05 -0700

    add a little basic componites and divs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
+import NavBar from './components/NavBar.jsx'
 
 class App extends React.Component {
   constructor(props){
     super(props);
-    this.state = {};
+    this.state = {
+      activeTab: 'default',
+    };
+    this.chooseActiveTab = this.chooseActiveTab.bind(this);
   }
 
   componentDidMount(){}
 
+  chooseActiveTab(tab){
+    const tabFunc = () => {
+      this.setState({activeTab:tab});
+    }
+    return tabFunc;
+  }
+
   render(){
     return(
-      <div>hello world!</div>
+      <div id="appBody">
+        <NavBar />
+      </div>
     )
   }
 }
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class NavBar extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {};
+  }
+
+  render(){
+    return(
+      <diV class = "navBar">words</diV>
+    )
+  }
+
+}
+
+export default NavBar;
