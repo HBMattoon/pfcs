e84f2e558a2e2f3180a750f670223153bd16b1c8
Henry Mattoon <hmattoon@mail.sfsu.edu>
2020-04-18 00:03:47 -0700

    add post, put and delete api endpoints

--- server/index.js
+++ server/index.js
@@ -6,12 +6,30 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 
+
+
 app.use('/', express.static(path.join(__dirname, './../client/dist')));
 
 app.get('/api/test', (req, res) => {
+  // res.status(200).send(data); //ok
+  res.status(404).end(); //not found
+});
 
+app.post('/api/test', (req, res) => {
+  // res.status(201).end(); //created
+  res.status(404).end();
 
-  res.end();
-})
+});
+
+app.put('/api/test', (req, res) => {
+  // res.status(200).end(); //ok
+  // res.status(204).end(); //no content
+  res.status(404).end(); //not found
+});
+
+app.delete('/api/test', (req, res) => {
+  // res.status(200).end(); //ok
+  res.status(404).end(); //not found
+});
 
 app.listen(port, ()=> (console.log('listening to port: ', port)));
